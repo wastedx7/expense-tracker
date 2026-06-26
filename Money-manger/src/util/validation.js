c1b57ca523ff00e49250e0fb60dca7f7472dfaa5
wastedx7 <wastedx7@gmail.com>
2026-06-27 03:42:43 +0530

    delete

--- Money-manger/src/util/validation.js
+++
@@ -1,7 +0,0 @@
-export const validEmail = (email) => {
-  if (email.trim()) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  }
-  return false;
-};
--- MoneyManagerApplication/src/main/java/com/danny/MoneyManagerApplication/service/Demo.java
+++
@@ -1,4 +0,0 @@
-package com.danny.MoneyManagerApplication.service;
-
-public class Demo {
-}

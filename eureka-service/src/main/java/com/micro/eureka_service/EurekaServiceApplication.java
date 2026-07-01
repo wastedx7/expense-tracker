296290082a22442d170830a4189a7733d7ef86c9
wastedx7 <wastedx7@gmail.com>
2026-07-01 16:54:38 +0530

    Add Eureka service and client configuration with necessary dependencies

---
+++ eureka-service/src/main/java/com/micro/eureka_service/EurekaServiceApplication.java
@@ -0,0 +1,14 @@
+package com.micro.eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
